ef6eb27a5c21f588643accbe873ad9e96f6dbc33
gabrielimarta <gabrielirocha2730@gmail.com>
2024-07-19 16:33:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+  import ProductList from './components/ProductList.vue';
+
+  const produtos = ref([
+    { id: 1, nome: 'Produto 1', preco: 19.99 },
+    { id: 2, nome: 'Produto 2', preco: 29.99 },
+    { id: 3, nome: 'Produto 3', preco: 39.99 },
+    {id: 4, nome: 'Produto 4', preco: 49.99 },
+    {id: 5, nome: 'Produto 5', preco: 59.99 }
+  ]);
+
+  // excluir elemento 
+  function removerItem(id) {
+    const index = produtos.value.findIndex((produto) => produto.id === id);
+    produtos.value.splice(index, 1);
+  }
+</script>
+
+<template>
+  <product-list :produtos="produtos" @remove="removerItem" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,52 @@
+<script setup>
+  const props = defineProps(['produtos']);
+</script>
+
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Preço</th>
+        <th>Ações</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="produto in props.produtos" :key="produto.id">
+        
+        <td>{{ produto.nome }}</td>
+        <td>{{ produto.preco }}</td>
+        <td>
+          <i class="mdi mdi-delete" @click="$emit('remove', produto.id)" />
+        </td>
+        
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+  table {
+    width: 50%;
+    border-collapse: collapse;
+  }
+
+  th,
+  td {
+    border: 1px solid rgb(113, 58, 145);
+    padding: 10px;
+  }
+
+  th {
+    background-color: rgb(108, 139, 151);
+  }
+
+  tr {
+    text-align: center;
+  }
+
+  tr i {
+    cursor: pointer;
+    font-size: larger;
+  }
+</style>
